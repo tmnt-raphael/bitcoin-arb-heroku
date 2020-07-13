379c1dfc8f3033ce4e3278c46e394a4243c7701f
Ken Luy <kvluy@yahoo.com>
2020-07-13 03:54:51 -0700

    delay 2 secs

--- myClock.py
+++ myClock.py
@@ -2,14 +2,16 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from rq import Queue
 from worker import conn
 from utils import count_words_at_url
+import time
 
 sched = BlockingScheduler()
 q = Queue(connection=conn)
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    result = q.enqueue(count_words_at_url, 'http://heroku.com')
+    job = q.enqueue(count_words_at_url, 'http://heroku.com')
 
-    print(result)
+    time.sleep(2)
+    print(job.result)
 
 sched.start()
